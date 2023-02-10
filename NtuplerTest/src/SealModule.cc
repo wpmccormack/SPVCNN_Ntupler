af8eb0892c6d125553a2736d3bf3ebfb9fc88e98
Patrick McCormack <54044012+wpmccormack@users.noreply.github.com>
2023-02-10 13:22:39 -0500

    initial commit

---
+++ NtuplerTest/src/SealModule.cc
@@ -0,0 +1,7 @@
+#include "FWCore/PluginManager/interface/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "NtuplerTest/NtuplerTest/interface/HcalTrigPrimDigiNtupler.h"
+#include "NtuplerTest/NtuplerTest/interface/HcalTrigPrimDigiNtuplerEXPANDEDINFO.h"
+
+DEFINE_FWK_MODULE(HcalTrigPrimDigiNtupler);
+DEFINE_FWK_MODULE(HcalTrigPrimDigiNtuplerEXPANDEDINFO);
